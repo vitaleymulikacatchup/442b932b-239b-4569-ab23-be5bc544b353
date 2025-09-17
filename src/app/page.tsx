a5f1379b52157783edb8ad88341aadaad2f5d904
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-09-17 19:17:16 +0300

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import SocialsAbout from '@/components/sections/layouts/about/SocialsAbout';
 import HowToBuy2D from '@/components/sections/layouts/howtobuy/2DHTB';
 import BigNumberTokenomics from '@/components/sections/layouts/tokenomics/BigNumberTokenomics';
 import FooterBase from '@/components/footer/FooterBase';
+import { Shield } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -46,7 +47,7 @@ export default function Home() {
           title="Tokenomics Overview"
           description="Understanding the key stats of BlackSpaceship Coin"
           kpiItems={[
-            { value: "100M", description: "Total Supply", longDescription: "Total amount of coins to be released.", icon: "TokenIcon" }
+            { value: "100M", description: "Total Supply", longDescription: "Total amount of coins to be released.", icon: Shield }
           ]} />
       </div>
 
